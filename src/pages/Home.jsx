b8f8e03bafff4e49d54846ba3c5d8594be8fda54
madays <maday.choque@gmail.com>
2023-11-30 09:40:11 -0500

    Carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import Title2 from "../components/Title2";
+import Carousel from "../components/Carousel";
 
 function Home() {
-
   return (
     <>
       <section>
         <Title2 title="Disney +" />
+        <Carousel />
       </section>
       <section>
         <Title2 title="Netflix" />
