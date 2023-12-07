a34a7ba23a9a2c7b143e1ccefc6f039afd715912
Victor Raul <v1c70rotakmer@gmail.com>
2023-12-07 15:10:31 -0500

    feat: add responsive to the techSheet component

--- src/components/TechSheet.jsx
+++ src/components/TechSheet.jsx
@@ -8,6 +8,7 @@ import {
 
 const TechSheet = () => {
 	const dataExample = {
+		title: 'Loki ',
 		imgCover:
 			'https://elcomercio.pe/resizer/b0JhIoRba8vhwoCG4c3tkfdvHuw=/400x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/YH52SIXPWZFNJH2UFT2TNT66HM.jpg',
 		sinopsis:
@@ -38,57 +39,104 @@ const TechSheet = () => {
 				character: 'Mobius M. Mobius',
 			},
 		],
+		platform: 'Disney+',
+		liked: false,
 	};
 	return (
-		<main className='mt-[48px] max-w-[1440px] lg:mt-[72px] mx-auto flex flex-col items-center py-4 px-6'>
-			<figure className='px-2'>
-				<img src={dataExample.imgCover} alt='foto de pelicula' />
-			</figure>
-			<a
-				href='#'
-				className='my-3 flex h-fit w-fit items-center justify-center gap-4 rounded-md border-[1px] border-[#4C4545] bg-[#ABA8AB] px-8 py-3 text-black'
-			>
-				<img src={playIcon} className='w-6' alt='' />
-				<p className='font-semibold uppercase'>Ver trailer</p>
-			</a>
-			<section className='rounded-[0.32rem] bg-white p-4'>
-				<div className='flex items-start justify-between'>
+		<main className='mx-auto mt-[48px] flex h-full w-full max-w-[1440px] flex-col items-center px-6 py-10 font-roboto md:px-10 lg:mt-[72px] lg:flex-row lg:items-center lg:gap-10 lg:px-14 lg:py-[2rem] xl:gap-20 xl:py-[5rem]'>
+			<div className='lg:flex-[1.7] xl:flex-[1.1]'>
+				<figure className='w-full'>
+					<img
+						src={dataExample.imgCover}
+						alt='foto de pelicula'
+						className='w-full'
+					/>
+				</figure>
+				<div className='mx-5 my-5 hidden items-center justify-center gap-3 bg-[#D9D9D9] p-2 lg:flex'>
 					<a href='#'>
-						<img src={likeIcon} alt='' className='w-8 h-8' />
+						<img src={likeIcon} alt='' className='h-8 w-8' />
 					</a>
 					<a href='#'>
-						<img src={dislikeIcon} alt='' className='w-8 h-8' />
+						<img src={dislikeIcon} alt='' className='h-8 w-8' />
 					</a>
 					<a href='#'>
-						<img src={tagIcon} alt='' className='w-8 h-8' />
+						<img src={tagIcon} alt='' className='h-8 w-8' />
 					</a>
 					<a href='#'>
-						<img src={bellIcon} alt='' className='w-8 h-8' />
-					</a>
-					<a href='#'>
-						<figure>
-							<img
-								src='https://sm.ign.com/ign_latam/cover/d/disney-plu/disney-plus_sxyh.jpg'
-								className='w-16 rounded-lg'
-								alt=''
-							/>
-						</figure>
+						<img src={bellIcon} alt='' className='h-8 w-8' />
 					</a>
 				</div>
-				<div>
-					<div className='py-6'>
-						<h3 className='font-bold'>SINOPSIS</h3>
-						<p className='text-sm'>{dataExample.sinopsis}</p>
+			</div>
+			<section className='lg:mb-20 lg:flex-[2.5] 2xl:mb-16'>
+				<div className='flex items-center justify-center gap-8 py-4 lg:justify-start'>
+					<div className='hidden items-center gap-12 lg:flex'>
+						<h2 className='text-4xl font-bold text-white'>
+							{dataExample.title}
+						</h2>
+						<a href='https://www.disneyplus.com/es-419/series/loki/6pARMvILBGzF'>
+							<figure>
+								<img
+									src='https://sm.ign.com/ign_latam/cover/d/disney-plu/disney-plus_sxyh.jpg'
+									className='w-16 rounded-lg'
+									alt=''
+								/>
+							</figure>
+						</a>
 					</div>
-					{/* cast */}
-					<div>
-						<h3 className='font-bold'>CAST</h3>
-						{dataExample.cast.map((item, index) => (
-							<p key={index} className='flex w-full mb-1 text-sm'>
-								<strong className='font-bold'>{item.name + ': '} </strong>
-								<p>{item.character}</p>
+					<a
+						href='https://www.youtube.com/watch?v=fM7v_IFaH1g'
+						className='border-gradient-rounded my-3 flex h-fit w-fit items-center justify-center gap-4 rounded-md px-8 py-3 text-black'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<img src={playIcon} className='w-6' alt='' />
+						<p className='font-semibold uppercase'>Ver trailer</p>
+					</a>
+				</div>
+				<div className='rounded-[0.32rem] bg-[#D9D9D9] p-4 lg:p-12'>
+					<div className='flex items-start justify-between lg:hidden'>
+						<a href='#'>
+							<img src={likeIcon} alt='' className='h-8 w-8' />
+						</a>
+						<a href='#'>
+							<img src={dislikeIcon} alt='' className='h-8 w-8' />
+						</a>
+						<a href='#'>
+							<img src={tagIcon} alt='' className='h-8 w-8' />
+						</a>
+						<a href='#'>
+							<img src={bellIcon} alt='' className='h-8 w-8' />
+						</a>
+						<a href='#'>
+							<figure>
+								<img
+									src='https://sm.ign.com/ign_latam/cover/d/disney-plu/disney-plus_sxyh.jpg'
+									className='w-16 rounded-lg'
+									alt=''
+								/>
+							</figure>
+						</a>
+					</div>
+					<div className='flex flex-col gap-6 lg:gap-9'>
+						<div>
+							<h3 className='font- text-lg'>SINOPSIS</h3>
+							<p className='text-sm font-semibold text-black'>
+								{dataExample.sinopsis}
 							</p>
-						))}
+						</div>
+						{/* cast */}
+						<div>
+							<h3 className='text-lg font-bold'>CAST</h3>
+							{dataExample.cast.map((item, index) => (
+								<p
+									key={index}
+									className='mb-1 flex w-full text-sm font-semibold'
+								>
+									{item.name + ': '}
+									{item.character}
+								</p>
+							))}
+						</div>
 					</div>
 				</div>
 			</section>
