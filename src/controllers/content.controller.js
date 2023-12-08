fab31c6e3e392b272fb5a63612724a8189d912f6
German <german.alejandrozulet@gmail.com>
2023-12-08 17:48:15 -0300

    removed unused space

--- src/controllers/content.controller.js
+++ src/controllers/content.controller.js
@@ -1,6 +1,5 @@
 import { getContentByPlatformService } from "../services/content.services.js"
 
-
 const getContentByPlatformController = async (req, res) => {
   const { network } = req.params// Aca va el id de la plataforma, se la pasaremos al controlador
   const selectedNetWork = network.toUpperCase()
