c07d2314e480ff61fb3b8bee542a3bc8b0f47c56
German <german.alejandrozulet@gmail.com>
2023-12-10 14:12:27 -0300

    implements handlebars and fixed bug in getting trending movies

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ import express from 'express'
 import cors from 'cors'
 import router from './routes/index.routes.js';
 import dotenv from 'dotenv'
-dotenv.config()
 import { mongoDB } from './config/mongoDB.js';
+import handlebars from 'express-handlebars'
+import __dirname from '../__dirname.js';
+
+dotenv.config()
 
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Handlebars
+
+app.set('view engine', 'hbs')
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  // handlebars: allowInsecurePrototypeAccess(Handlebars)
+}))
+
+app.use(express.static('public'))
 
 // Middleware
 app.use(cors({
@@ -24,7 +39,9 @@ app.use(cors({
 }));
 
 app.use('/api/v1', router)
+app.set('views', __dirname + '/src/views')
 
+// setup DB
 await mongoDB()
 
 const httpServer = app.listen(PORT, () => console.log("Escuchando en el puerto 4000 "))
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,12 +3,13 @@ import contentRoutes from './content.routes.js'
 import authRoutes from './auth.routes.js'
 import listRoutes from './lists.routes.js'
 import { checkJWT } from "../middlewares/session.middleware.js";
-
+import viewsRoutes from './views.routes.js'
 const router = Router()
 
 router.use('/trending', contentRoutes)
 router.use('/auth', authRoutes)
 router.use('/favourites', checkJWT, listRoutes)
+router.use('/', viewsRoutes)
 
 
 
