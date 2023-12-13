d6a1863ece89bbe38953dab781aaeb7cde8d6756
German <german.alejandrozulet@gmail.com>
2023-12-12 21:57:31 -0300

    testing cors

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['*'],
 }));
 
 app.use('/api/v1', router)
